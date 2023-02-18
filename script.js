29167462485a5677d7ce520f764fda32ecd0617d
badzlan <badzlandhabith05@gmail.com>
2023-02-18 11:36:55 +0700

    add looping and if else

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ alert('GAME TEBAK ANGKA! \nTebak Angka 1 - 10 \nKamu Punya 3 Kesemapatan');
 
 const angkaYangBenar = Math.floor(Math.random() * 10) + 1;
 
+for (let nyawa = 3; nyawa >= 1; nyawa--) {
+
+    const angkaTebakan = prompt('Masukkan tebakan angka');
+
+    if(angkaTebakan == angkaYangBenar){
+        alert(`Tebakan anda BENAR! \nAngka yang dicari adalah : ${angkaYangBenar}`);
+        break;
+    } else if(angkaTebakan > angkaYangBenar){
+        alert(`Tebakan anda terlalu TINGGI! \nAnda masih punya ${nyawa - 1} kesempatan`);
+    } else if(angkaTebakan < angkaYangBenar){
+        alert(`Tebakan anda terlalu RENDAH! \nAnda masih punya ${nyawa - 1} kesempatan`);
+    }
+
+}
