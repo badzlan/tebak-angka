dc2ba33a5ce2ec3d3d1c63a2c2b5be0a74ce8ca8
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2023-03-02 21:29:16 +0700

    fix first alert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert('GAME TEBAK ANGKA! \nTebak Angka 1 - 10 \nKamu Punya 3 Kesemapatan');
+alert('GAME TEBAK ANGKA! \nTebak Angka 1 - 10 \nKamu Punya 3 Kesempatan');
 
 const angkaYangBenar = Math.floor(Math.random() * 10) + 1;
 
